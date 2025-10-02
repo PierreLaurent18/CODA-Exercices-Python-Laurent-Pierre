5c4924ec0cdeab8c84ab7021f6a7a3eb0ff39c0f
pierre laurent <pierrelau1308@gmail.com>
2025-10-02 14:07:23 +0200

    exercice4

--- main.py
+++ main.py
@@ -54,6 +54,10 @@ def exercice12():
     for i in range(5):
         print("Bonjour !")
 
+def exercice13():
+    for i in range(1,6):
+        print(i)
+
 
 
 
@@ -84,6 +88,8 @@ def main():
         exercice11()
     elif choix == "12":
         exercice12()
+    elif choix == "13":
+        exercice13()
     else:
         print("Exercice non reconnu.")
     
